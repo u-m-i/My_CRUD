764db5d9f0c04c3b4fdfbd501f895e7cebca3bd9
umi <asiessoyeltier1@gmail.com>
2021-04-24 11:22:21 -0500

    Corregimos varios erroes los arguemntos en el método DictReader fielname por fieldnames y el parámetro mode con la correcta indicación con un string y no con una variable.

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ CLIENT_TABLE = ".clients.csv"
 CLIENT_SCHEMA = ['name','company','email', 'position']
 clients = []
 def _initialize_clients_from_storage():
-    with open(CLIENT_TABLE, mode=r) as f:
-        reader = csv.DictReader(f, fieldname=CLIENT_SCHEMA)
+    with open(CLIENT_TABLE, mode='r') as f:
+        reader = csv.DictReader(f, fieldnames=CLIENT_SCHEMA)
         for row in reader:
             clients.append(row)
 
 
 def _save_clients_to_storage():
     tmp_table_name = f'{CLIENT_TABLE}.tmp'
-    with open(tmp_table_name, mode=w) as f:
-        writer = csv.DictWriter(f, field=CLIENT_SCHEMA)
+    with open(tmp_table_name, mode='w') as f:
+        writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
         writer.writerows(clients)
         os.remove(CLIENT_TABLE)
         os.rename(tmp_table_name, CLIENT_TABLE)
