0266d930a62158b8fe29308b6df65402d9842fa1
umi <asiessoyeltier1@gmail.com>
2021-04-27 11:36:10 -0500

    Creamos la función list_client, para generar un listado de los clientes que hay.

--- clients/services.py
+++ clients/services.py
@@ -11,4 +11,10 @@ class ClientService:
         with open(self.table_name, mode='a') as f:
             writer = csv.DictWriter(f, fieldnames=Client.schema())
             writer.writerow(client.to_dict())
+    
+
+    def list_client(self):
+        with open(self.table_name, mode='r') as f:
+            reader = csv.DictReader(f, fieldnames=Client.schema())
+            return list(reader)
             
