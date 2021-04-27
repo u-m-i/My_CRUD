51a0aed355d6afcd7c80b29dddec9c1deb4d7bca
umi <asiessoyeltier1@gmail.com>
2021-04-27 07:30:04 -0500

    Creamos nuestro comando clients, con el decorador click.group, luego nombramos a cada función como un comando que pertenece al comando clients junto con el contexto.

--- clients/commands.py
+++ clients/commands.py
@@ -1,26 +1,38 @@
 import click
 
+@click.group()
 def clients():
-    """Manages the client life cicle
+    """Manages the client lifecycle
     """
     pass
 
+
+@clients.command() #Pasamos el comando cliente, para agregarlo a su  grupo de comandos,jutno con el contexto
+@click.pass_context()
 def create(ctx, name, company, email, position):
     """Creates a new client 
     """
-    pass
+    pass#Siempre dejamos dos espacios entre funciones
 
+
+@clients.command()
+@click.pass_context()
 def list(ctx):
-    """List All clients
+    """List all clients
     """
     pass
 
+
+@clients.command()
+@click.pass_context()
 def updated(ctx, client_cid):
     """Updates a client
     """
     pass
 
 
+@clients.command()
+@click.pass_context()
 def delete(ctx, client_cid):
     """Deletes a client
     """
