c2d24c3e690f34a927502c0d3ba8032b10f49b8d
umi <asiessoyeltier1@gmail.com>
2021-04-27 07:21:03 -0500

    En commands.py tenemos nuestro primeras funciones vacías, pero sin ningún decorador

---
+++ clients/commands.py
@@ -0,0 +1,27 @@
+import click
+
+def clients():
+    """Manages the client life cicle
+    """
+    pass
+
+def create(ctx, name, company, email, position):
+    """Creates a new client 
+    """
+    pass
+
+def list(ctx):
+    """List All clients
+    """
+    pass
+
+def updated(ctx, client_cid):
+    """Updates a client
+    """
+    pass
+
+
+def delete(ctx, client_cid):
+    """Deletes a client
+    """
+    pass
