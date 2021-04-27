75509cba6346772ce5e70530d7405bb428479e8d
umi <asiessoyeltier1@gmail.com>
2021-04-27 11:15:27 -0500

    Declaramos en pv.py una constante con nuestro archivo csv para luego utilizar el ctx como objeto al cuál de asignamos nuestro archivo, del cuál todos los comandos pueden afectar.

--- pv.py
+++ pv.py
@@ -3,10 +3,12 @@ import click #Importamos click.
  #desde nuestro módulo clients importamos los comandos que creamos, le decimos a commands que se llame clients_commands
 from clients import commands as clients_commands
 
+CLIENTS_TABLE = '.clients.csv'
+
 @click.group() #inicializamos
 @click.pass_context #pasamos un contexto
 def cli(ctx):
-    ctx.obj = {} #Creamos el ctx como un diccionario vacío. 
-
+    ctx.obj = {}#Creamos el ctx como un diccionario vacío.
+    ctx.obj['clients_table'] = CLIENTS_TABLE  
 
 cli.add_command(clients_commands.all)
